8c725e298055f7d76e2e9a860426223811dae07f
daniel <4954577+jaensen@users.noreply.github.com>
2024-09-06 01:36:37 +0200

    New way to query circles balances enriched with token information from the V_Crc_Tokens view.

--- Circles.Index.Rpc/ICirclesRpcModule.cs
+++ Circles.Index.Rpc/ICirclesRpcModule.cs
@@ -9,9 +9,14 @@ namespace Circles.Index.Rpc;
 
 #region DTOs
 
-public record CirclesTokenBalance(Address Token, string Balance, string TokenOwner);
-
-public record CirclesTokenBalanceV2(UInt256 TokenId, string Balance, string TokenOwner);
+public record CirclesTokenBalance(
+    UInt256 TokenId,
+    string TokenAddress,
+    string Balance,
+    string TokenOwner,
+    string TokenType,
+    string InflationaryBalance,
+    int Version);
 
 public record CirclesTrustRelation(Address User, int limit);
 
@@ -25,7 +30,10 @@ public record CirclesEvent(string Event, IDictionary<string, object?> Values);
 public interface ICirclesRpcModule : IRpcModule
 {
     [JsonRpcMethod(Description = "Gets the V1 Circles balance of the specified address", IsImplemented = true)]
-    Task<ResultWrapper<string>> circles_getTotalBalance(Address address, bool asTimeCircles = false);
+    Task<ResultWrapper<string>> circles_getTotalBalance(Address address, bool? asTimeCircles = true); 
+    
+    [JsonRpcMethod(Description = "Gets the V2 Circles balance of the specified address", IsImplemented = true)]
+    Task<ResultWrapper<string>> circlesV2_getTotalBalance(Address address, bool? asTimeCircles = true);
 
     [JsonRpcMethod(Description = "This method allows you to query all (v1) trust relations of an address",
         IsImplemented = true)]
@@ -33,15 +41,7 @@ public interface ICirclesRpcModule : IRpcModule
 
     [JsonRpcMethod(Description = "Gets the balance of each V1 Circles token the specified address holds",
         IsImplemented = true)]
-    Task<ResultWrapper<CirclesTokenBalance[]>> circles_getTokenBalances(Address address, bool asTimeCircles = false);
-
-    [JsonRpcMethod(Description = "Gets the V2 Circles balance of the specified address", IsImplemented = true)]
-    Task<ResultWrapper<string>> circlesV2_getTotalBalance(Address address, bool asTimeCircles = false);
-
-    [JsonRpcMethod(Description = "Gets the balance of each V2 Circles token the specified address holds",
-        IsImplemented = true)]
-    Task<ResultWrapper<CirclesTokenBalanceV2[]>>
-        circlesV2_getTokenBalances(Address address, bool asTimeCircles = false);
+    Task<ResultWrapper<CirclesTokenBalance[]>> circles_getTokenBalances(Address address);
 
     [JsonRpcMethod(Description = "Queries the data of one Circles index table",
         IsImplemented = true)]
