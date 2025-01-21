8989ce3697d7711c7ff39b1abfad4b272629363b
daniel <4954577+jaensen@users.noreply.github.com>
2025-01-21 21:24:47 +0100

    feat: parse LBPFactory events

---
+++ Circles.Index.CirclesV2.LBP/LogParser.cs
@@ -0,0 +1,142 @@
+using Circles.Index.Common;
+using Nethermind.Core;
+using Nethermind.Core.Crypto;
+using Nethermind.Core.Extensions;
+
+namespace Circles.Index.CirclesV2.LBP;
+
+public class LogParser(Address factoryAddress) : ILogParser
+{
+    private readonly Hash256 _circlesBackingDeployedTopic = new(DatabaseSchema.CirclesBackingDeployed.Topic);
+    private readonly Hash256 _lbpDeployedTopic = new(DatabaseSchema.LBPDeployed.Topic);
+    private readonly Hash256 _circlesBackingInitiatedTopic = new(DatabaseSchema.CirclesBackingInitiated.Topic);
+    private readonly Hash256 _circlesBackingCompletedTopic = new(DatabaseSchema.CirclesBackingCompleted.Topic);
+
+    public IEnumerable<IIndexEvent> ParseTransaction(Block block, int transactionIndex, Transaction transaction)
+    {
+        yield break;
+    }
+
+    public IEnumerable<IIndexEvent> ParseLog(
+        Block block,
+        Transaction transaction,
+        TxReceipt receipt,
+        LogEntry log,
+        int logIndex)
+    {
+        if (log.Topics.Length == 0)
+        {
+            yield break;
+        }
+
+        if (log.Address != factoryAddress)
+        {
+            yield break;
+        }
+
+        var topic = log.Topics[0];
+
+        if (topic == _circlesBackingDeployedTopic)
+        {
+            yield return CirclesBackingDeployed(block, receipt, log, logIndex);
+        }
+        else if (topic == _lbpDeployedTopic)
+        {
+            yield return LbpDeployed(block, receipt, log, logIndex);
+        }
+        else if (topic == _circlesBackingInitiatedTopic)
+        {
+            yield return CirclesBackingInitiated(block, receipt, log, logIndex);
+        }
+        else if (topic == _circlesBackingCompletedTopic)
+        {
+            yield return CirclesBackingCompleted(block, receipt, log, logIndex);
+        }
+    }
+
+    // event CirclesBackingDeployed(address indexed backer, address indexed circlesBackingInstance);
+    private CirclesBackingDeployed CirclesBackingDeployed(Block block, TxReceipt receipt, LogEntry log, int logIndex)
+    {
+        var backer = "0x" + log.Topics[1].ToString().Substring(Consts.AddressEmptyBytesPrefixLength);
+        var circlesBackingInstance = "0x" + log.Topics[2].ToString().Substring(Consts.AddressEmptyBytesPrefixLength);
+
+        return new CirclesBackingDeployed(
+            block.Number,
+            (long)block.Timestamp,
+            receipt.Index,
+            logIndex,
+            receipt.TxHash!.ToString(),
+            backer,
+            circlesBackingInstance
+        );
+    }
+
+    // event LBPDeployed(address indexed circlesBackingInstance, address indexed lbp);
+    private LbpDeployed LbpDeployed(Block block, TxReceipt receipt, LogEntry log, int logIndex)
+    {
+        var circlesBackingInstance = "0x" + log.Topics[1].ToString().Substring(Consts.AddressEmptyBytesPrefixLength);
+        var lbp = "0x" + log.Topics[2].ToString().Substring(Consts.AddressEmptyBytesPrefixLength);
+
+        return new LbpDeployed(
+            block.Number,
+            (long)block.Timestamp,
+            receipt.Index,
+            logIndex,
+            receipt.TxHash!.ToString(),
+            circlesBackingInstance,
+            lbp
+        );
+    }
+
+    // event CirclesBackingInitiated(address indexed backer, address indexed circlesBackingInstance, address backingAsset, address personalCirclesAddress);
+    private CirclesBackingInitiated CirclesBackingInitiated(Block block, TxReceipt receipt, LogEntry log, int logIndex)
+    {
+        var backer = "0x" + log.Topics[1].ToString().Substring(Consts.AddressEmptyBytesPrefixLength);
+        var circlesBackingInstance = "0x" + log.Topics[2].ToString().Substring(Consts.AddressEmptyBytesPrefixLength);
+
+        var data = log.Data;
+        var backingAssetBytes = data[0..32];
+        var personalCirclesAddressBytes = data[32..64];
+
+        var backingAsset = "0x" + BitConverter
+            .ToString(backingAssetBytes[12..32])
+            .Replace("-", "")
+            .ToLowerInvariant();
+
+        var personalCirclesAddress = "0x" + BitConverter
+            .ToString(personalCirclesAddressBytes[12..32])
+            .Replace("-", "")
+            .ToLowerInvariant();
+
+        return new CirclesBackingInitiated(
+            block.Number,
+            (long)block.Timestamp,
+            receipt.Index,
+            logIndex,
+            receipt.TxHash!.ToString(),
+            backer,
+            circlesBackingInstance,
+            backingAsset,
+            personalCirclesAddress
+        );
+    }
+
+    // event CirclesBackingCompleted(address indexed backer, address indexed circlesBackingInstance, address lbp);
+    private CirclesBackingCompleted CirclesBackingCompleted(Block block, TxReceipt receipt, LogEntry log, int logIndex)
+    {
+        var backer = "0x" + log.Topics[1].ToString().Substring(Consts.AddressEmptyBytesPrefixLength);
+        var circlesBackingInstance = "0x" + log.Topics[2].ToString().Substring(Consts.AddressEmptyBytesPrefixLength);
+        var lbp = "0x" + log.Data.ToHexString().Substring(Consts.AddressEmptyBytesPrefixLength);
+
+        return new CirclesBackingCompleted(
+            block.Number,
+            (long)block.Timestamp,
+            receipt.Index,
+            logIndex,
+            receipt.TxHash!.ToString(),
+            backer,
+            circlesBackingInstance,
+            lbp
+        );
+    }
+}
