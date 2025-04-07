8729eb373b5b15b84b625baf8b8df742e4237e76
jaensen <4954577+jaensen@users.noreply.github.com>
2025-04-07 21:47:08 +0200

    feat: carry along the following additional properties in the BalanceNode: IsWrapped, IsStatic

--- Circles.Pathfinder/Nodes/BalanceNode.cs
+++ Circles.Pathfinder/Nodes/BalanceNode.cs
@@ -1,11 +1,12 @@
 namespace Circles.Pathfinder.Nodes;
 
-public class BalanceNode(string address, string token, long amount, bool isWrapped = false)
+public class BalanceNode(string address, string token, long amount, bool isWrapped, bool isStatic)
     : Node(address + "-" + token)
 {
     public string Token { get; } = token;
     public long Amount { get; } = amount;
     public bool IsWrapped { get; } = isWrapped;
+    public bool IsStatic { get; } = isStatic;
 
     public string HolderAddress => Address.Split("-")[0];
 }
