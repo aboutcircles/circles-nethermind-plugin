f96bee9134b09fdb613da9331d42f82d394fa1a7
jaensen <4954577+jaensen@users.noreply.github.com>
2025-04-07 21:47:27 +0200

    feat: carry along the following additional properties in the BalanceNode: IsWrapped, IsStatic

--- Circles.Pathfinder/Graphs/BalanceGraph.cs
+++ Circles.Pathfinder/Graphs/BalanceGraph.cs
@@ -11,29 +11,22 @@ public class BalanceGraph : IGraph<CapacityEdge>
     public IDictionary<string, BalanceNode> BalanceNodes { get; } = new Dictionary<string, BalanceNode>();
     public IDictionary<string, AvatarNode> AvatarNodes { get; } = new Dictionary<string, AvatarNode>();
 
-    private readonly List<string>? _fromTokens;
-    private readonly string? _sourceAddress;
-    private readonly List<string>? _toTokens;
-    private readonly string? _sinkAddress;
-    private readonly bool? _withWrap;
-
-
     public void AddAvatar(string avatarAddress)
     {
         Nodes.Add(avatarAddress, new AvatarNode(avatarAddress));
     }
 
-    public void AddBalance(string address, string token, long balance, bool isWrapped = false)
+    public void AddBalance(string address, string token, long balance, bool isWrapped, bool isStatic)
     {
         address = address.ToLower();
         token = token.ToLower();
-        
+
         if (!AvatarNodes.ContainsKey(address))
         {
             AvatarNodes.Add(address, new AvatarNode(address));
         }
 
-        var balanceNode = new BalanceNode(address, token, balance, isWrapped);
+        var balanceNode = new BalanceNode(address, token, balance, isWrapped, isStatic);
         Nodes.Add(balanceNode.Address, balanceNode);
         BalanceNodes.Add(balanceNode.Address, balanceNode);
 
