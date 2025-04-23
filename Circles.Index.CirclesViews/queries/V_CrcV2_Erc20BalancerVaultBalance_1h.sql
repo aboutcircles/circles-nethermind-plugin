195935077e1c00bb9bbb565fbced6c82fa954cca
hdser <hugo.serodio@gnosis.io>
2025-04-23 15:13:21 +0000

    add Erc20BalancerVaultBalance

---
+++ Circles.Index.CirclesViews/queries/V_CrcV2_Erc20BalancerVaultBalance_1h.sql
@@ -0,0 +1,73 @@
+-- COLUMNS:
+-- timestamp:ValueTypes.BigInt:true
+-- tokenAddress:ValueTypes.Address:true
+-- value:ValueTypes.BigInt:true
+
+
+create or replace view public."V_CrcV2_Erc20BalancerVaultBalance_1h"
+    ("timestamp", "tokenAddress", "value") as
+WITH
+
+balancer_vault_diffs AS (
+	SELECT
+		date_trunc('hour',TO_TIMESTAMP("timestamp"))  AS "timestamp"
+		,"tokenAddress"
+		,SUM("vaultBalanceDiff") AS "vaultBalanceDiff"
+	FROM (
+		SELECT 
+			"timestamp"
+			,"tokenAddress"
+			,amount AS "vaultBalanceDiff"
+		FROM "CrcV2_Erc20WrapperTransfer"
+		WHERE 
+			"to" = LOWER('0xBA12222222228d8Ba445958a75a0704d566BF2C8')
+		UNION ALL
+		SELECT 
+			"timestamp"
+			,"tokenAddress"
+			,-amount AS "vaultBalanceDiff"
+		FROM "CrcV2_Erc20WrapperTransfer"
+		WHERE 
+			"from" = LOWER('0xBA12222222228d8Ba445958a75a0704d566BF2C8')
+	)
+	GROUP BY 1, 2
+),
+
+min_timestamp_tokenAddress AS (
+    SELECT
+        "tokenAddress",
+        MIN("timestamp") AS min_timestamp
+    FROM 
+        balancer_vault_diffs
+    GROUP BY 1
+),
+
+calendar AS (
+    SELECT
+        g."tokenAddress",
+        generate_series(
+            g.min_timestamp,
+            date_trunc('hour', CURRENT_TIMESTAMP),
+            interval '1 hour'
+        ) AS "timestamp"
+    FROM min_timestamp_tokenAddress g
+),
+
+balancer_vault_diffs_dense AS (
+    SELECT 
+	    t1."tokenAddress",
+	    t1."timestamp" ,
+	    COALESCE(t2."vaultBalanceDiff", 0) AS "vaultBalanceDiff"
+	FROM 
+	    calendar t1
+	LEFT JOIN 
+		balancer_vault_diffs t2
+	    ON t1."tokenAddress" = t2."tokenAddress" AND t1."timestamp" = t2."timestamp"
+)
+
+SELECT
+	"timestamp"
+	,"tokenAddress"
+	,SUM("vaultBalanceDiff") OVER (PARTITION BY "tokenAddress" ORDER BY "timestamp") AS value
+FROM
+	balancer_vault_diffs_dense
