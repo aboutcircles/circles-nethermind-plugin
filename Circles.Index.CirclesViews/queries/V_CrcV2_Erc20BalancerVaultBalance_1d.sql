1197856fc2ff5fbb6b0ffeb25a35b046ea0c31bb
hdser <hugo.serodio@gnosis.io>
2025-06-03 16:13:17 +0000

    optimize daily resolution queries

--- Circles.Index.CirclesViews/queries/V_CrcV2_Erc20BalancerVaultBalance_1d.sql
+++ Circles.Index.CirclesViews/queries/V_CrcV2_Erc20BalancerVaultBalance_1d.sql
@@ -8,17 +8,66 @@ create or replace view public."V_CrcV2_Erc20BalancerVaultBalance_1d"
     ("timestamp", "tokenAddress", "value") as
 WITH
 
-vault_balance AS (
-  SELECT 
-    date_trunc('day', "timestamp") AS "timestamp",
-	"tokenAddress",
-    ARRAY_AGG(value ORDER BY "timestamp" DESC) AS values
-  FROM "V_CrcV2_Erc20BalancerVaultBalance_1h"
-  GROUP BY 1, 2
+balancer_vault_diffs AS (
+	SELECT
+		date_trunc('day',TO_TIMESTAMP("timestamp"))  AS "timestamp"
+		,"tokenAddress"
+		,SUM("vaultBalanceDiff") AS "vaultBalanceDiff"
+	FROM (
+		SELECT 
+			"timestamp"
+			,"tokenAddress"
+			,amount AS "vaultBalanceDiff"
+		FROM "CrcV2_Erc20WrapperTransfer"
+		WHERE 
+			"to" = LOWER('0xBA12222222228d8Ba445958a75a0704d566BF2C8')
+		UNION ALL
+		SELECT 
+			"timestamp"
+			,"tokenAddress"
+			,-amount AS "vaultBalanceDiff"
+		FROM "CrcV2_Erc20WrapperTransfer"
+		WHERE 
+			"from" = LOWER('0xBA12222222228d8Ba445958a75a0704d566BF2C8')
+	)
+	GROUP BY 1, 2
+),
+
+min_timestamp_tokenAddress AS (
+    SELECT
+        "tokenAddress",
+        MIN("timestamp") AS min_timestamp
+    FROM 
+        balancer_vault_diffs
+    GROUP BY 1
+),
+
+calendar AS (
+    SELECT
+        g."tokenAddress",
+        generate_series(
+            g.min_timestamp,
+            date_trunc('day', CURRENT_TIMESTAMP),
+            interval '1 day'
+        ) AS "timestamp"
+    FROM min_timestamp_tokenAddress g
+),
+
+balancer_vault_diffs_dense AS (
+    SELECT 
+	    t1."tokenAddress",
+	    t1."timestamp" ,
+	    COALESCE(t2."vaultBalanceDiff", 0) AS "vaultBalanceDiff"
+	FROM 
+	    calendar t1
+	LEFT JOIN 
+		balancer_vault_diffs t2
+	    ON t1."tokenAddress" = t2."tokenAddress" AND t1."timestamp" = t2."timestamp"
 )
 
-SELECT 
-  "timestamp",
-  "tokenAddress",
-  values[1] AS value 
-FROM vault_balance;
+SELECT
+	"timestamp"
+	,"tokenAddress"
+	,SUM("vaultBalanceDiff") OVER (PARTITION BY "tokenAddress" ORDER BY "timestamp") AS value
+FROM
+	balancer_vault_diffs_dense
--- Circles.Index.CirclesViews/queries/V_CrcV2_GroupMembersCount_1d.sql
+++ Circles.Index.CirclesViews/queries/V_CrcV2_GroupMembersCount_1d.sql
@@ -4,19 +4,100 @@
 -- value:ValueTypes.Int:true
 
 create or replace view "V_CrcV2_GroupMembersCount_1d" ("group", "timestamp", "value") as
-WITH
 
-members_1d AS (
-  SELECT 
-    "group",
-    date_trunc('day', "timestamp") AS "timestamp",
-    ARRAY_AGG(value ORDER BY "timestamp" DESC) AS values
-  FROM "V_CrcV2_GroupMembersCount_1h"
-  GROUP BY 1, 2
+WITH 
+
+---- GroupMembersChange
+groups_trusts AS (
+	SELECT
+		t1."timestamp",
+		t1."logIndex",
+		t1.truster,
+		t1.trustee,
+		t1."expiryTime",
+		LEAD(t1."timestamp") OVER (PARTITION BY t1.truster, t1.trustee ORDER BY t1."timestamp", t1."logIndex") AS next_ts
+	FROM 
+		"CrcV2_Trust" t1
+	INNER JOIN 
+		"V_CrcV2_Groups" t2 
+		ON t2.group = t1.truster 
+),
+
+trust_intervals AS (
+	SELECT 
+		truster AS "group",
+		"timestamp" AS start_ts,
+		CASE 
+			WHEN "expiryTime" < 10000000000 THEN "expiryTime"
+			WHEN next_ts IS NOT NULL THEN next_ts
+			ELSE NULL -- Still valid
+		END AS end_ts
+	FROM groups_trusts
+),
+
+group_membership_changes AS (
+	SELECT 
+		start_ts AS "timestamp",
+		"group",
+		1 AS cnt
+	FROM trust_intervals
+
+	UNION ALL
+
+	SELECT 
+		end_ts AS "timestamp",
+		"group",
+		-1 AS cnt
+	FROM trust_intervals
+	WHERE end_ts IS NOT NULL
+),
+
+-- V_CrcV2_GroupMembersCount_1h
+members_hourly_sparse AS (
+    SELECT 
+        date_trunc('day',TO_TIMESTAMP("timestamp")) AS "timestamp"
+        ,"group"
+        ,SUM(cnt) AS cnt
+    FROM 
+        group_membership_changes
+	GROUP BY 1, 2
+),
+
+min_max_per_group AS (
+    SELECT
+        "group",
+        MIN("timestamp") AS min_timestamp
+    FROM 
+        members_hourly_sparse
+    GROUP BY 1
+),
+
+calendar AS (
+    SELECT
+        g."group",
+        generate_series(
+            g.min_timestamp,
+            date_trunc('day', CURRENT_TIMESTAMP),
+            interval '1 day'
+        ) AS "timestamp"
+    FROM min_max_per_group g
+),
+
+members_change AS (
+    SELECT 
+	    t1."group",
+	    t1."timestamp" ,
+	    COALESCE(t2.cnt, 0) AS cnt
+	FROM 
+	    calendar t1
+	LEFT JOIN 
+		members_hourly_sparse t2
+	    ON t1."group" = t2."group" AND t1."timestamp" = t2."timestamp"
 )
 
+
 SELECT 
-  "group",
-  "timestamp",
-  values[1] AS value 
-FROM members_1d;
+    "group",
+    "timestamp",
+    SUM(cnt) OVER (PARTITION BY "group" ORDER BY "timestamp") AS value
+FROM members_change;
