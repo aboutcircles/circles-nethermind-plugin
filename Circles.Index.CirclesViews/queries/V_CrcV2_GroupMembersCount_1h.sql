018fbf56993b2f92c9492c364ea551ed41a8fc7f
jaensen <jaensen>
2025-09-19 04:30:43 +0200

    feat: fixed a few queries + allow search to filter for avatarType

--- Circles.Index.CirclesViews/queries/V_CrcV2_GroupMembersCount_1h.sql
+++ Circles.Index.CirclesViews/queries/V_CrcV2_GroupMembersCount_1h.sql
@@ -3,101 +3,71 @@
 -- timestamp:ValueTypes.BigInt:true
 -- value:ValueTypes.Int:true
 
-create or replace view "V_CrcV2_GroupMembersCount_1h" ("group", "timestamp", "value") as
-
-WITH 
-
----- GroupMembersChange
-groups_trusts AS (
-	SELECT
-		t1."timestamp",
-		t1."logIndex",
-		t1.truster,
-		t1.trustee,
-		t1."expiryTime",
-		LEAD(t1."timestamp") OVER (PARTITION BY t1.truster, t1.trustee ORDER BY t1."timestamp", t1."logIndex") AS next_ts
-	FROM 
-		"CrcV2_Trust" t1
-	INNER JOIN 
-		"V_CrcV2_Groups" t2 
-		ON t2.group = t1.truster 
+create or replace view public."V_CrcV2_GroupMembersCount_1h"("group","timestamp",value) as
+with groups_trusts as (
+    select
+        t1."timestamp"::bigint                                   as start_epoch,
+        t1."logIndex"::bigint                                    as log_index,
+        t1.truster                                               as "group",
+        t1.trustee                                               as trustee,
+        t1."expiryTime"::numeric                                 as expiry_numeric,
+        lead(t1."timestamp"::bigint) over (
+            partition by t1.truster, t1.trustee
+            order by t1."timestamp"::bigint, t1."logIndex"::bigint
+        )                                                        as next_epoch
+    from "CrcV2_Trust" t1
+    join "V_CrcV2_Groups" t2 on t2."group" = t1.truster
 ),
-
-trust_intervals AS (
-	SELECT 
-		truster AS "group",
-		"timestamp" AS start_ts,
-		CASE 
-			WHEN "expiryTime" < 10000000000 THEN "expiryTime"
-			WHEN next_ts IS NOT NULL THEN next_ts
-			ELSE NULL -- Still valid
-		END AS end_ts
-	FROM groups_trusts
+trust_intervals as (
+    select
+        gt."group",
+        gt.start_epoch,
+        case
+            when gt.expiry_numeric < 10000000000 then
+                case when gt.next_epoch is not null then least(gt.expiry_numeric::bigint, gt.next_epoch)
+                     else gt.expiry_numeric::bigint end
+            else gt.next_epoch
+        end                                                      as end_epoch_exclusive
+    from groups_trusts gt
+    where gt.next_epoch is null or gt.next_epoch > gt.start_epoch
 ),
-
-group_membership_changes AS (
-	SELECT 
-		start_ts AS "timestamp",
-		"group",
-		1 AS cnt
-	FROM trust_intervals
-
-	UNION ALL
-
-	SELECT 
-		end_ts AS "timestamp",
-		"group",
-		-1 AS cnt
-	FROM trust_intervals
-	WHERE end_ts IS NOT NULL
+membership_changes as (
+    select ti."group", ti.start_epoch as change_epoch,  1  as delta from trust_intervals ti
+    union all
+    select ti."group", ti.end_epoch_exclusive,        -1  as delta from trust_intervals ti
+    where ti.end_epoch_exclusive is not null
 ),
-
--- V_CrcV2_GroupMembersCount_1h
-members_hourly_sparse AS (
-    SELECT 
-        date_trunc('hour',TO_TIMESTAMP("timestamp")) AS "timestamp"
-        ,"group"
-        ,SUM(cnt) AS cnt
-    FROM 
-        group_membership_changes
-	GROUP BY 1, 2
+changes_hourly as (
+    select
+        -- bucket to UTC hours, keep timestamptz
+        (date_trunc('hour', (to_timestamp(mc.change_epoch) at time zone 'UTC')) at time zone 'UTC')::timestamptz as "timestamp",
+        mc."group",
+        sum(mc.delta) as delta
+    from membership_changes mc
+    group by 1, mc."group"
 ),
-
-min_max_per_group AS (
-    SELECT
-        "group",
-        MIN("timestamp") AS min_timestamp
-    FROM 
-        members_hourly_sparse
-    GROUP BY 1
+range_per_group as (
+    select "group", min("timestamp") as min_ts_utc
+    from changes_hourly
+    group by "group"
 ),
-
-calendar AS (
-    SELECT
-        g."group",
-        generate_series(
-            g.min_timestamp,
-            date_trunc('hour', CURRENT_TIMESTAMP),
-            interval '1 hour'
-        ) AS "timestamp"
-    FROM min_max_per_group g
+calendar as (
+    select
+        r."group",
+        gs::timestamptz as "timestamp"
+    from range_per_group r
+    cross join generate_series(
+        r.min_ts_utc,
+        (date_trunc('hour', (current_timestamp at time zone 'UTC')) at time zone 'UTC')::timestamptz,
+        interval '1 hour'
+    ) as gs
 ),
-
-members_change AS (
-    SELECT 
-	    t1."group",
-	    t1."timestamp" ,
-	    COALESCE(t2.cnt, 0) AS cnt
-	FROM 
-	    calendar t1
-	LEFT JOIN 
-		members_hourly_sparse t2
-	    ON t1."group" = t2."group" AND t1."timestamp" = t2."timestamp"
+dense as (
+    select c."group", c."timestamp", coalesce(h.delta,0) as delta
+    from calendar c
+    left join changes_hourly h
+      on h."group" = c."group" and h."timestamp" = c."timestamp"
 )
-
-
-SELECT 
-    "group",
-    "timestamp",
-    SUM(cnt) OVER (PARTITION BY "group" ORDER BY "timestamp") AS value
-FROM members_change;
+select "group","timestamp",
+       sum(delta) over (partition by "group" order by "timestamp") as value
+from dense;
--- Circles.Index.CirclesViews/queries/V_CrcV2_GroupMemberships.sql
+++ Circles.Index.CirclesViews/queries/V_CrcV2_GroupMemberships.sql
@@ -10,34 +10,29 @@
 -- memberType:ValueTypes.String:true
 
 create or replace view public."V_CrcV2_GroupMemberships"
-            ("blockNumber", timestamp, "transactionIndex", "logIndex", "transactionHash", "group", member, "expiryTime",
-             "memberType")
+("blockNumber", "timestamp", "transactionIndex", "logIndex", "transactionHash", "group", member, "expiryTime", "memberType")
 as
-with cte as (SELECT t."blockNumber",
-                    t."timestamp",
-                    t."transactionIndex",
-                    t."logIndex",
-                    t."transactionHash",
-                    t.truster                                                   AS "group",
-                    t.trustee                                                   AS member,
-                    t."expiryTime",
-                    case
-                        when h.avatar is not null then 'CrcV2_RegisterHuman'
-                        when o.organization is not null then 'CrcV2_RegisterOrganization'
-                        when g.group is not null then 'CrcV2_RegisterGroup' end AS "memberType"
-             FROM "V_CrcV2_TrustRelations" t
-                      JOIN "CrcV2_RegisterGroup" g ON t.truster = g."group"
-                      LEFT JOIN "CrcV2_RegisterHuman" h on h.avatar = t.trustee
-                      LEFT JOIN "CrcV2_RegisterOrganization" o on o.organization = t.trustee
-                      LEFT JOIN "CrcV2_RegisterGroup" gg on gg.group = t.trustee
+with cte as (
+    select
+        t."blockNumber",
+        t."timestamp",
+        t."transactionIndex",
+        t."logIndex",
+        t."transactionHash",
+        t.truster as "group",
+        t.trustee as member,
+        t."expiryTime",
+        case
+            when h.avatar is not null then 'CrcV2_RegisterHuman'
+            when o.organization is not null then 'CrcV2_RegisterOrganization'
+            when gg."group" is not null then 'CrcV2_RegisterGroup'
+            else null
+        end as "memberType"
+    from "V_CrcV2_TrustRelations" t
+    join "CrcV2_RegisterGroup" g on g."group" = t.truster         -- ensures truster is a group
+    left join "CrcV2_RegisterHuman" h on h.avatar = t.trustee      -- is member a human?
+    left join "CrcV2_RegisterOrganization" o on o.organization = t.trustee -- org?
+    left join "CrcV2_RegisterGroup" gg on gg."group" = t.trustee   -- is member a group?
 )
-select "blockNumber",
-       timestamp,
-       "transactionIndex",
-       "logIndex",
-       "transactionHash",
-       "group",
-       member,
-       "expiryTime",
-       "memberType"
+select "blockNumber","timestamp","transactionIndex","logIndex","transactionHash","group",member,"expiryTime","memberType"
 from cte;
