498c383de70228748578d194b623ae1b5281fc47
jaensen <jaensen>
2025-11-03 15:45:48 +0100

    fix/wip: Also use block when SyncMode == WaitingForBlock

--- Circles.Index/Plugin.cs
+++ Circles.Index/Plugin.cs
@@ -337,9 +337,14 @@ public class Plugin : INethermindPlugin
                     {
                         // Should handle blocks in the following sync modes:
                         case SyncMode.Full:
+                        case SyncMode.WaitingForBlock:
                         case SyncMode.DbLoad:
+                            _indexerContext.Logger.Info(
+                                $"New head block while syncingInfo is SyncMode.Full or SyncMode.DbLoad. New head will be processed.");
                             break;
                         default:
+                            _indexerContext.Logger.Warn(
+                                $"New head block while syncingInfo not SyncMode.Full or SyncMode.DbLoad. New head will be skipped. Current sync-status: {syncingInfo.ToString()}");
                             return;
                     }
                 }
